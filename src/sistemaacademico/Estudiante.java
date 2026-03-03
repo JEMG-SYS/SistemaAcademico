470a7189594d18bcbd7edc20c508593073bdd889
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-03 07:26:57 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/sistemaacademico/Estudiante.java
+++ src/sistemaacademico/Estudiante.java
@@ -9,5 +9,21 @@ package sistemaacademico;
  * @author 2jcue
  */
 public class Estudiante {
+    private String codigo;
+    private String nombre;
+    private String programa;
+    private int semestre;
+    
+    // Constructor vacío
+    public Estudiante() {
+    }
+
+    // Constructor con todos los atributos
+    public Estudiante(String codigo, String nombre, String programa, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.programa = programa;
+        this.semestre = semestre;
+    }
     
 }
